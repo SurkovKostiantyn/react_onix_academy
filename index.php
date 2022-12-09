7849277f7cfbc2a1ee73b4a2991f485cc1eae359
Kostiantyn Surkov <kskrua@gmail.com>
2022-12-09 17:46:53 +0200

    fix

--- index.php
+++ index.php
@@ -13,13 +13,15 @@
         header('Pragma: no-cache'); // HTTP 1.0.
         header('Expires: 0'); // Proxies.
         echo "<link rel='stylesheet' href='style/main.css?v=$version'>";
-        echo "<script src='js/sudoku.min.js?v=$version'></script>";
     ?>
     <script src='js/fontawesome.js'></script>
     <link rel="icon" href="files/ico.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Hanalei+Fill&family=Montserrat:wght@100;300;500;700&family=Rubik&display=swap" rel="stylesheet"></head>
+    <link href="https://fonts.googleapis.com/css2?family=Hanalei+Fill&family=Montserrat:wght@100;300;500;700&family=Rubik&display=swap" rel="stylesheet">
+    <script src='js/sudoku.min.js'></script>
+    <script src='js/fontawesome.js'></script>
+</head>
 <body>
 </body>
 </html>
