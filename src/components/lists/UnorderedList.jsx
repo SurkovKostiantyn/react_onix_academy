eb091793650656cb9ef0d06592cd8ded5589ffaf
ETI-activestudents <eti.activestudents@gmail.com>
2023-02-23 10:39:32 +0200

    -key

--- src/components/lists/UnorderedList.jsx
+++ src/components/lists/UnorderedList.jsx
@@ -1,19 +1,11 @@
 import { Component } from "react";
 export default class UnorderedList extends Component{
     // eslint-disable-next-line no-useless-constructor
-    constructor(props) {
-        super(props);
-        this.state = {
-            className: this.props.className,
-            listArr: this.props.list
-        }
-    }
     render(){
-        const {className, listArr } = this.state;
-        let key = 0;
+        const {className, list } = this.props;
         return(
             <ul className={className}>
-                {listArr.map(el=> <li key={key++}>{el}</li>)}
+                {list.map(el=> <li>{el}</li>)}
             </ul>
         )
     }
