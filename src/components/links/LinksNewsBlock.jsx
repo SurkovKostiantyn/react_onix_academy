7e7a374af559186efd313bc5110f97a8e46f79be
ETI-activestudents <eti.activestudents@gmail.com>
2023-02-23 10:41:23 +0200

    this.state -> this.props

--- src/components/links/LinksNewsBlock.jsx
+++ src/components/links/LinksNewsBlock.jsx
@@ -1,16 +1,8 @@
 import { Component } from "react";
 
 export default class LinksNewsBlock extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            href: this.props.href,
-            className: this.props.className,
-            itemsList: this.props.itemsList
-        }
-    }
     render(){
-        const {href,className,itemsList} = this.state;
+        const {href,className,itemsList} = this.props;
         return(
             <a href={href} className={className}>
                 {itemsList.map(el => el)}
