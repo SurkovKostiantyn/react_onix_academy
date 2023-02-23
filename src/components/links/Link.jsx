970f235db4c48d091666618d3570de3a7dfa5b2d
ETI-activestudents <eti.activestudents@gmail.com>
2023-02-23 10:36:43 +0200

    this.state -> this.props

--- src/components/links/Link.jsx
+++ src/components/links/Link.jsx
@@ -1,16 +1,8 @@
 import { Component } from "react";
 export default class Link extends Component{
     // eslint-disable-next-line no-useless-constructor
-    constructor(props) {
-        super(props);
-        this.state = {
-            href: props.href,
-            className: this.props.className,
-            innerHTML: this.props.innerHTML
-        }
-    }
     render(){
-        const {href, className, innerHTML } = this.state;
+        const {href, className, innerHTML } = this.props;
         return(
             <a href={href} className={className}>{innerHTML}</a>
         )
