b5441adf2496cf86562ec47a88fdaa630de27113
ETI-activestudents <eti.activestudents@gmail.com>
2023-02-23 10:41:42 +0200

    this.state -> this.props

--- src/components/elements/Image.jsx
+++ src/components/elements/Image.jsx
@@ -1,16 +1,8 @@
 import { Component } from "react";
 export default class Image extends Component{
     // eslint-disable-next-line no-useless-constructor
-    constructor(props) {
-        super(props);
-        this.state = {
-            src: props.src,
-            className: this.props.className,
-            alt: this.props.alt
-        }
-    }
     render(){
-        const {src, className, alt } = this.state;
+        const {src, className, alt } = this.props;
         return(
             <img src={src} className={className} alt={alt}/>
         )
