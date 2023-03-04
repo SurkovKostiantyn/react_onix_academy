ec63d7e990e447bc732d2a443e81f4de290923b2
ETI-activestudents <eti.activestudents@gmail.com>
2023-03-05 00:41:47 +0200

    use react FontAwesome

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import ReactDOMClient from 'react-dom/client';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faTwitter, faFacebook, faYoutube, faPinterest, faBehanceSquare
+} from '@fortawesome/free-brands-svg-icons';
 import Home from './pages/Home';
+
+library.add(faTwitter, faFacebook, faYoutube, faPinterest, faBehanceSquare);
 
 ReactDOMClient
   .createRoot(document.getElementById('root'))
