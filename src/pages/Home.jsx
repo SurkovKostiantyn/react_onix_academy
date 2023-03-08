a4a79e58e1a3104f8dde589a94bce417bb6b6f54
ETI-activestudents <eti.activestudents@gmail.com>
2023-03-08 14:59:17 +0200

    Потерпел крах с react-intersection-observer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,49 @@
 import '../styles/main.css';
-import React from 'react';
-
+import React, { Component } from 'react';
 import Nav from '../components/blocks/Nav';
 import Banner from '../components/blocks/Banner';
 import ContentNews from '../components/blocks/news/ContentNews';
 import ContentBelow from '../components/blocks/news/ContentBelow';
 import Footer from '../components/blocks/Footer';
 
-export default function Home() {
-  const BannerRef = React.createRef();
+export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isInView: null,
+      BannerRef: React.createRef()
+    };
+  }
 
-  let isInView = false;
+  componentDidMount() {
+    const callback = ([entry]) => {
+      const { isIntersecting } = entry;
+      this.setState({ isInView: !isIntersecting });
+    };
 
-  const callback = ([entry]) => {
-    const { isIntersecting } = entry;
-    isInView = !isIntersecting;
-  };
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.0
+    };
 
-  const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.0
-  };
+    const observer = new IntersectionObserver(callback, options);
 
-  const observer = new IntersectionObserver(callback, options);
+    observer.observe(
+      this.BannerRef
+    );
+  }
 
-  observer.observe(
-    BannerRef.current
-  );
-
-  return (
-    <>
-      <Nav isInView={isInView} />
-      <Banner RefProp={BannerRef} />
-      <ContentNews />
-      <ContentBelow />
-      <Footer />
-    </>
-  );
+  render() {
+    const { isInView, BannerRef } = this.state;
+    return (
+      <>
+        <Nav isInView={isInView} />
+        <Banner RefProp={BannerRef} />
+        <ContentNews />
+        <ContentBelow />
+        <Footer />
+      </>
+    );
+  }
 }
